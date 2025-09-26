68b1e9b0be04a8f52c70f10a8ef66d2a78f12ba2
moyu-hax <simplething168@outlook.com>
2025-09-26 11:08:01 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -22,7 +22,7 @@ const SITE_CONFIG = {
 
 // API站点配置
 const API_SITES = {
-    dyttzy: {
+dyttzy: {
     api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
     name: '电影天堂',
     detail: 'http://caiji.dyttzyapi.com',
